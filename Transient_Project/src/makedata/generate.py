abc8ba8c1ea2b6875c521db020f9d6d4b0b6767b
Mountebank6 <theo.faridani@gmail.com>
2018-02-20 14:01:01 -0800

    Added replacedata and fixed some bugs
    replacedata lets you replace the data in the ImageData class without
    requiring that the new data is the same shape as the old data.
    fixed swapping x and y
    fixed size being confused with shape
    changed required data type from uint8 to uint16
    fixed poor choice regarding checking == vs is
    updated docstrings

--- Transient_Project/src/makedata/generate.py
+++ Transient_Project/src/makedata/generate.py
@@ -15,34 +15,68 @@ class ImageData:
     def __init__(self, xdim=0, ydim=0):
         """Create array of zeros with a given shape.
         
-        Only rectangular data allowed.
+        Keyword arguments:
+        xdim -- the width of the image array (default 0)
+        ydim -- the height of the image array (default 0)
         """
         
         self.x = xdim
         self.y = ydim
-        self.data = np.zeros((self.x,self.y),dtype=np.uint8)
+        self.data = np.zeros((self.y,self.x),dtype=np.uint16)
     
     def fill(self, newdata=None):
-        """Change self.data to a new, given array of the same size."""
-        if newdata == None:
-            newdata = np.zeros((self.x,self.y),dtype=np.uint8)
-        if newdata.dtype != np.dtype('uint8'):
-            raise TypeError('Newdata has wrong datatype. Needs uint8.')
-        if newdata.size != self.data.size:
-            raise IndexError('Size of new data is different than ' + 
-                             'size of old data')
+        """Change self.data to a new, given array of the same shape.
+        
+        Keyword arguments:
+        newdata -- the new numpy ndarray of image data. Must have same
+                   shape as self.data.
+        
+        If no newdata is given (i.e. x.fill() is called), then it sets
+        self.data to an array of zeros of the appropriate shape)
+        """
+        if newdata is None:
+            newdata = np.zeros((self.y,self.x),dtype=np.uint16)
+        if isinstance(newdata, np.ndarray) == False:
+            raise TypeError("newdata is not of type numpy.ndarray")
+        if newdata.shape != self.data.shape:
+            raise IndexError("shape of new data is different than " + 
+                             "shape of old data")
+        if newdata.dtype != np.uint16:
+            raise TypeError("Newdata has wrong datatype in its " +
+                            "entries. Has " + str(newdata.dtype) +
+                            " Needs uint16.")
         self.data = newdata
         
     def change(self, deltas=None):
         """Change self by giving a list of what should be changed.
         
-        The deltas argument must be formatted as a list where each
-        entry is another list of length 3. In these length 3 lists,
-        indexes 0 and 1 describe what index in self.data should be 
-        changed, and index 2 gives the value it should be changed to.
+        Keyword arguments:
+        deltas -- A list of ordered pairs associated with new values.
+        Each entry in the deltas list has the following format:
+        [tuple, value] where the tuple is (ylocation,xlocation) for
+        the value to be placed.
         """
-        if deltas == None:
+        if deltas is None:
             deltas = []
         for i in xrange(len(deltas)):
-            self.data[deltas[i][0],deltas[i][1]] = deltas[i][2]
+            self.data[deltas[i][0]] = deltas[i][1]
             
+    def replacedata(self, newdata=None):
+        """Replace self.data with new data of different shape  
+        
+        This funcionality is kept intentionally separate from 
+        ImageData.fill to prevent accidental changing of array shape.
+        
+        Keyword arguments:
+        newdata -- the new numpy ndarray of image data. Can
+                   have different shape than current data.
+        """
+        if isinstance(newdata, np.ndarray) == False:
+            raise TypeError("newdata is not of type numpy.ndarray")
+        if newdata.dtype != np.uint16:
+            raise TypeError("Newdata has wrong datatype in its " +
+                            "entries. Has " + str(newdata.dtype) +
+                            " Needs uint16.")
+        self.y = newdata.shape[0]
+        self.x = newdata.shape[1]
+        self.data = newdata
