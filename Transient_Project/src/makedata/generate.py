8308be795cafc03b92a7595538c37dc82fb23804
Mountebank6 <theo.faridani@gmail.com>
2018-03-01 18:43:25 -0800

    Added initial population generation
    Still needs to edit the data with an event intensity. Number of
    parameters is climbing quickly and I grow nervous

--- Transient_Project/src/makedata/generate.py
+++ Transient_Project/src/makedata/generate.py
@@ -5,10 +5,9 @@ Generate fake sample data for testing.
 """
 
 from __future__ import division
-from blaze.expr.expressions import shape
 
 __author__ = "Theo Faridani"
-__version__ = "0.08"
+__version__ = "0.09"
 
 import numpy as np
 import astropy.nddata as nd
@@ -17,7 +16,7 @@ _allowed_types = [np.uint8, np.uint16, np.uint32, np.uint64,
                  np.float16, np.float32, np.float64]
 
 def check_shape(shape):
-    """Check shape's types and value"""
+    """Throw errors if types/values are wrong"""
     if not isinstance(shape, tuple):
         raise TypeError("Bad operand type for shape: " 
                         + str(type(shape)))
@@ -25,28 +24,28 @@ def check_shape(shape):
         raise ValueError("shape is not of length 2")
     
 def check_data_type(data_type):
-    """Check data_type's types and value"""
+    """Throw errors if types/values are wrong"""
     if data_type not in _allowed_types:
         raise TypeError("Bad operand type for data_type: " 
                         + str(type(data_type)) + "\n Must be"
                         + "one of " + str(_allowed_types))
             
 def check_delta_t(delta_t):
-    """Check delta_t's types and value"""
+    """Throw errors if types/values are wrong"""
     if not (isinstance(delta_t, float) 
             or isinstance(delta_t, int)):
         raise TypeError("Bad operand type for delta_t: " + str(type(delta_t))
                         + "\n Must be a python float or python int")
         
 def check_event_rate(event_rate):
-    """Check event_rate's types and value"""
+    """Throw errors if types/values are wrong"""
     if not (isinstance(event_rate, float)
             or isinstance(event_rate, np.ndarray)):
         raise TypeError("Bad operand type for event_rate: "
                         + str(type(event_rate)) + "\n Must be" 
                         + " float or numpy array of same shape as shape")
 def check_n(n):
-    """Check n's types and value"""
+    """Throw errors if types/values are wrong"""
     if not isinstance(n, int):
         raise TypeError("Bad operand type for n: "
                         + str(type(n)) + "\n Must be int")
@@ -57,13 +56,26 @@ class TransientSeries:
     """
     
     
-    def new_population(self, shape, event_rate, event_lifetime, data_type):
+    def new_population(self):
         """Generate fresh transient population and clear old one"""
-        blank_image = nd.NDDataRef(np.zeros(shape, dtype=data_type))
-   
+        blank_image = nd.NDDataRef(np.zeros(self.shape, dtype=self.data_type))
+        self.current_event_locations = []
+        self.current_event_births = []
+        self.current_event_lifetimes = []
+        if isinstance(self.event_rate, float):
+            if self.event_rate <= 1 and self.event_rate >= 0:
+                for i in range(len(blank_image.data)):
+                    for k in range(len(blank_image.data[i])):
+                        if np.random.rand() < self.event_rate:
+                            self.current_event_births.append(0.0)
+                            self.current_event_locations.append((i,k))
+                            self.current_event_lifetimes.append(
+                                np.random.normal(self.event_lifetime,
+                                                 self.event_lifetime_sigma))
+        
     def __init__(self, shape=None, delta_t=None,
-                 n=None, event_rate=None, event_lifetime=None,
-                 data_type=np.uint16):
+                 n=None, event_rate=None, event_lifetime=None, 
+                 event_lifetime_sigma=1e-100, data_type=np.uint16):
         """Generate initial conditions from parameters.
         """
         
@@ -73,6 +85,7 @@ class TransientSeries:
         self.event_rate = event_rate
         self.data_type = data_type
         self.event_lifetime = event_lifetime
+        self.event_lifetime_sigma = event_lifetime_sigma
         
         if self.shape is None:
             raise TypeError("Required argument 'shape' (pos 1) not found")
