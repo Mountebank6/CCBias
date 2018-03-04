2dd0cac02a2f1d262d7d400ef01f9dd1c8eeac93
Mountebank6 <theo.faridani@gmail.com>
2018-03-04 12:08:35 -0800

    added checking on parameter values (e.g. time can't be negative)
    Also added randomization of initial event birth times. Question: since
    rate has units of probability of an event per pixel per delta_t, does it
    make sense to use rate to detirmine *initial* event population?

--- Transient_Project/src/makedata/generate.py
+++ Transient_Project/src/makedata/generate.py
@@ -24,20 +24,23 @@ class TransientSeries:
         """Generate fresh transient population and clear old one"""
         self.current_event_locations = []
         self.current_event_births = []
-        self.current_lifetimes = []
-        if isinstance(self.rate, float):
-            if self.rate <= 1 and self.rate >= 0:
-                for i in range(len(self.astro_data.data)):
-                    for k in range(len(self.astro_data.data[i])):
-                        if np.random.rand() < self.rate:
-                            self.current_event_births.append(0.0)
-                            self.current_event_locations.append((i,k))
-                            self.current_lifetimes.append(
-                                np.random.normal(self.lifetime,
-                                                 self.lifetime_sigma))
+        self.current_remaining_life = []
+        for i in range(len(self.astro_data.data)):
+            for k in range(len(self.astro_data.data[i])):
+                if np.random.rand() < self.rate:
+                    if self.lifetime_sigma is not None:
+                        lifetime = np.random.normal(self.lifetime,
+                                             self.lifetime_sigma)
+                    else:
+                        lifetime = self.lifetime
+                    birth = np.random.uniform(-lifetime,0)
+                    self.current_event_births.append(birth)
+                    self.current_event_locations.append((i,k))
+                    self.current_remaining_life.append(0.0 - birth)
+                        
     
     def set_intensity_guassian(self,mag,sigma):
-        """Give new events guassian intensity"""
+        """Give new events gaussian intensity"""
         for index in self.current_event_locations:
             if self.astro_data.data[index] == 0:
                 self.astro_data.data[index] = np.random.normal(mag,sigma)
@@ -56,8 +59,9 @@ class TransientSeries:
         self.data_type = data_type
         self.lifetime = lifetime
         self.lifetime_sigma = lifetime_sigma
-        self.astro_data = nd.NDDataRef(np.zeros(self.shape, 
-                                                dtype=self.data_type))
+        self.gauss_intensity = gauss_intensity
+        self.gauss_sigma = gauss_sigma
+        self.astro_data = nd.NDDataRef(np.zeros(self.shape, self.data_type))
         
         if self.shape is None:
             raise TypeError("Required argument 'shape' (pos 1) not found")
@@ -69,12 +73,14 @@ class TransientSeries:
         check_delta_t(self.delta_t)
         check_rate(self.rate)
         check_n(self.n)
-        check_lifetime_sigma(lifetime_sigma)
-        #TODO: CHECK INTENSITY AND SIGMA
+        check_lifetime_sigma(self.lifetime_sigma)
+        check_gauss_intensity(self.gauss_intensity)
+        check_gauss_sigma(self.gauss_sigma)
+#TODO: Current code throws errors if some values are at their default
+#    of none. Make sure this is really what you want.
         self.new_population()
         if (gauss_intensity is not None) and (gauss_sigma is not None):
             self.set_intensity_guassian(gauss_intensity, gauss_sigma)
-        #TODO: write better code
     
 def check_shape(shape):
     """Throw errors if types/values are wrong"""
@@ -83,6 +89,9 @@ def check_shape(shape):
                         + str(type(shape)))
     if len(shape) != 2:
         raise ValueError("shape is not of length 2")
+    for x in shape:
+        if not isinstance(x, int):
+            raise TypeError("shape tuple must contain ints")
     
 def check_data_type(data_type):
     """Throw errors if types/values are wrong"""
@@ -95,8 +104,10 @@ def check_delta_t(delta_t):
     """Throw errors if types/values are wrong"""
     if not (isinstance(delta_t, float) 
             or isinstance(delta_t, int)):
-        raise TypeError("Bad operand type for delta_t: " + str(type(delta_t))
+        raise TypeError("Bad operand type for delta_t: " + str(type(delta_t))  
                         + "\n Must be a python float or python int")
+    if delta_t <= 0:
+        raise ValueError("delta_t must be >0")
         
 def check_rate(rate):
     """Throw errors if types/values are wrong"""
@@ -105,15 +116,43 @@ def check_rate(rate):
         raise TypeError("Bad operand type for rate: "
                         + str(type(rate)) + "\n Must be" 
                         + " float or numpy array of same shape as shape")
+    if isinstance(rate, float):
+        if rate > 1 or rate < 0:
+            raise ValueError("rate is not a probability. " + 
+                             "Must be between 0 and 1 inclusive")
 def check_n(n):
     """Throw errors if types/values are wrong"""
-    if not isinstance(n, int):
-        raise TypeError("Bad operand type for n: "
-                        + str(type(n)) + "\n Must be int") 
+    if n is not None:
+        if not isinstance(n, int):
+            raise TypeError("Bad operand type for n: "
+                            + str(type(n)) + "\n Must be int") 
+        if n < 1:
+            raise ValueError("n must be >= 1")
 
 def check_lifetime_sigma(lifesigma):
-    if not isinstance(lifesigma, float):
-        raise TypeError("Bad operand type for lifetime_sigma: "
-                        + str(type(lifesigma))) + "\n Must be float"
-        
-        
+    """Throw errors if types/values are wrong"""
+    if lifesigma is not None:
+        if not isinstance(lifesigma, float):
+            raise TypeError("Bad operand type for lifetime_sigma: "
+                            + str(type(lifesigma))) + "\n Must be float"
+        if lifesigma <= 0:
+            raise ValueError("Sigma of event lifetime is <= 0. Bad!")
+
+def check_gauss_intensity(inten):
+    """Throw errors if types/values are wrong"""
+    if inten is not None:
+        if not isinstance(inten, float):
+            raise TypeError("Bad operand type for gauss_intensity: "
+                            + str(type(inten))) + "\n Must be float"
+        if inten < 0:
+            raise ValueError("Intensity must be >= 0")
+
+def check_gauss_sigma(sigm):
+    """Throw errors if types/values are wrong"""
+    if sigm is not None:
+        if not isinstance(sigm, float):
+            raise TypeError("Bad operand type for gauss_sigma: "
+                            + str(type(sigm))) + "\n Must be float"
+        if sigm <= 0:
+            raise ValueError("Sigma of event intensity is <= 0. Bad!")
+                                
