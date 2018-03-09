d796eaec29204ad52b393268713e138720bd2a1c
Mountebank6 <theo.faridani@gmail.com>
2018-03-09 08:44:03 -0800

    git is weird

---
+++ Transient_Project/src/makedata/tools/velmaker.py
@@ -0,0 +1,24 @@
+"""
+Generate random velocities projected onto the sky
+
+"""
+
+import numpy as np
+import math
+
+__all__ = ['get_velocity']
+
+vels = []
+for i in range(100000):
+    x = np.random.rand()
+    y = np.random.rand()
+    z = np.random.rand()
+    mag = math.sqrt(x**2+y**2+z**2)
+    x = x/mag
+    y = y/mag
+    z = z/mag
+    vels.append((x,y))
+
+def get_unifv():
+    """Return a random element of the uniform velocity distribution"""
+    return vels[np.random.randint(0,100000)]
