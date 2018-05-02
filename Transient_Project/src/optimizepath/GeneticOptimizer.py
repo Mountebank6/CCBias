644fe5f877b397d18f786d99a541c02a83ba77f5
Mountebank6 <theo.faridani@gmail.com>
2018-05-01 17:24:31 -0700

    little bit more of GA stuff

--- Transient_Project/src/optimizepath/GeneticOptimizer.py
+++ Transient_Project/src/optimizepath/GeneticOptimizer.py
@@ -5,15 +5,18 @@ Use a score function to optimize search path using a genetic algorithm
 """
 
 import numpy as np
+from collections import namedtuple
 from ..makedata import TransientSeries as tr
 from .opttools import scoring
 from .opttools import searchpath
 from ..makedata.tools import FOV
 
-def GeneticOptimizer(generator, crossrate, mutrate, popsize, iterations):
+def GeneticOptimizer(generator, crossrate, mutrate, popsize, iterations, 
+                     scoring_function, scoring_function_extra_args):
     """Return optimized search paths for a given TransientSeries"""
     genomelength = generator.get_n()
     searchspace = generator.get_shape()
+    point = namedtuple("Point", "x y")
     if crossrate > 1 or crossrate < 0:
         raise ValueError("crossrate is of inappropriate value")
     if mutrate > 1 or mutrate < 0:
@@ -25,10 +28,10 @@ def GeneticOptimizer(generator, crossrate, mutrate, popsize, iterations):
     for _ in range(popsize):
         genome = []
         for _ in range(genomelength):
-            genome.append([
+            genome.append(point(
                             np.random.randint(0,searchspace[0]-1),
                             np.random.randint(0,searchspace[1]-1)
-                          ])
+                          ))
         population.append(genome)
     
 
