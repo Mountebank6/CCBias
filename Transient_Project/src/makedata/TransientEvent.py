d2a9d15078e44ecf2e65725f88dfc6a8b5155c2d
Mountebank6 <theo.faridani@gmail.com>
2018-08-11 15:06:34 -0700

    Streamlined TransientEvent

--- Transient_Project/src/makedata/TransientEvent.py
+++ Transient_Project/src/makedata/TransientEvent.py
@@ -4,40 +4,46 @@ Class to hold an individual event
 """
 
 import numpy as np
+import copy
 
 class TransientEvent:
     """
     """
-    def __init__(self, loc, lifetime, birthtime, luminositySeries = None,  ):
+    def __init__(self, loc, lifetime, luminositySeries = None,  ):
         """
         Arguments:
-            loc: iterable of tuples. First position is starting
-                location. Second is velocity
+            loc: length 5 list. Positions:
+                0: birth time
+                1: birth x-location
+                2: birth y-location
+                3: x-velocity
+                4: y-velocity                
             lifetime: float. Number of frames
-            birthtime: float. Time of birth
             luminositySeries: Iterable of luminosities as fractions of 
                 maximum reportable luminosity. The ith entry in the
                 iterable represents the luminosity on the ith frame
                 of its existance
         """
-        self.loc = loc[0]
-        if len(loc) > 1:
-                self.velocity = loc[1]
+        self.loc = loc
+        self.history = [copy.copy(loc)]
         self.lifetime = lifetime
-        self.birthtime = birthtime
         if isinstance(luminositySeries, None):
             self.lum = 1
             self.luminositySeries = [1]
         else:
             self.luminositySeries = luminositySeries
             self.lum = self.luminositySeries[0]
-        self.currentTime = 0
         self.markedForDeath = False
     
     def advance(self):
-        self.currentTime += 1
-        self.lum = self.luminositySeries[self.currentTime]
-        self.loc[0] += self.velocity[0]
-        self.loc[1] += self.velocity[1]
-        if self.currentTime - self.birthtime > self.lifetime:
+        if not self.markedForDeath:
+            self.loc[0] += 1
+            self.loc[1] += self.loc[3]
+            self.loc[2] += self.loc[4]
+            self.history.append(copy.copy(self.loc) + [self.lum])
+            self.lum = (self.luminositySeries[
+                                self.loc[0] % len(self.luminositySeries)
+                                             ])
+        
+        if self.loc[0] - self.history[0][0] > self.lifetime:
             self.markedForDeath = True
