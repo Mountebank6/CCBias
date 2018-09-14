0536396a03c37184b393b7bd2f4384d90fbcbcb7
Mountebank6 <theo.faridani@gmail.com>
2018-09-14 11:06:11 -0700

    preliminary stuff for breeding. [WIP]

--- Transient_Project/src/optimizepath/TransientGenetic.py
+++ Transient_Project/src/optimizepath/TransientGenetic.py
@@ -2,15 +2,20 @@ import numpy as np
 import random
 from copy import copy, deepcopy
 
+
 """To do this we will require the setup of the
 TransientSurvey object to take in ranges for the different
 variables. This will allow the construction of the
 characterized genome"""
 
+binomial = np.random.binomial
+sample = random.sample
+
 class TransientGenetic:
     """Class that optimizes survey score with genetic algorithm"""
 
-    def __init__(self, survey, scoringFunc, surveyTime):
+    def __init__(self, survey, scoringFunc, surveyTime,
+                 popSize, mutRate, crossRate, iterations):
         """
         Args:
             survey:
@@ -32,4 +37,45 @@ class TransientGenetic:
         self.score = scoringFunc
         self.runTime = surveyTime
         self.surv = survey
+        self.crossRate = crossRate
+        self.mutRate = mutRate
+        self.iterations = iterations
+        self.charGenome = (self.vChar
+                         + self.oChar
+                         + self.hChar
+                         + self.sChar)
+        self.genomeLength = len(self.charGenome)
         return
+
+    def breed(self, mother, father):
+        """Return child-genomes from crossover and mutation"""
+        #apologies for heteronormativity
+        if len(mother) != len(father):
+            raise IndexError("Genome lengths mismatched")
+        
+        #Get number of crossovers and mutations to be applied
+        crossovers = binomial(len(mother), self.crossRate)
+        mutations = binomial(len(mother), self.mutRate)
+
+        #Get where in the genome they occur
+            #here we're choosing that all locations in the genome
+            #are equally probable locations for mutation/crossover
+        #The definition of geneNumbers is for readability
+        geneNumbers = range(self.genomeLength)
+        
+        #Get where in the genome the crossover will occur
+        #Sort the locations for iteration further on
+        crossLocations = sample(geneNumbers, crossovers)
+        crossLocations.sort()
+        
+        child1 = mother
+        child2 = father
+        for i in range(crossovers):
+            if i % 2 == 0:
+                child1[i:] = father[i:]
+                child2[i:] = mother[i:]
+            else:
+                child1[i:] = mother[i:]
+                child2[i:] = father[i:]
+
+
