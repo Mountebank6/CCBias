143d76c1fbff54828db6b3d29f0d409527cba561
Sister_Centipede <theo.faridani@gmail.com>
2019-02-05 13:29:59 -0800

    listified the optionmenu handling

--- Transient_Project/src/GUI/TransientGUI.py
+++ Transient_Project/src/GUI/TransientGUI.py
@@ -55,7 +55,12 @@ class CCBias():
         self.bla = self.userFuncs.keys()
         
         (self.vFieldPath, self.eObsPath, 
-         self.holDetectPath, self.measureFilePath) = 4*[None]    
+         self.holDetectPath, self.measureFilePath) = 4*[None]  
+
+        self.oPStringVars = []
+        self.oPSelectorLabels = []
+        self.selectOPOptionMenus = []
+        self.oPTraceFuncs = []  
         
         
         self.createWidgets()
@@ -121,8 +126,8 @@ class CCBias():
         This is ATROCIOUS but since it's just GUI stuff I guess
         it's okay.
         """
-        OPMaker = ttk.Frame(parent)
-        parent.add(OPMaker, text='Observing Profile Maker')
+        self.OPMaker = ttk.Frame(parent)
+        parent.add(self.OPMaker, text='Observing Profile Maker')
         vFieldFunc = tk.StringVar()
         EObsFunc = tk.StringVar()
         HolDetectFunc = tk.StringVar()
@@ -130,55 +135,33 @@ class CCBias():
         MeasureFunc = tk.StringVar()
         vFieldFunc.set("default")
 
-        loadFile = tk.Button(OPMaker, text="Upload File",
+        loadFile = tk.Button(self.OPMaker, text="Upload File",
                     command = lambda: addFile(getFile()))
 
-        self.oPStringVars = []
-        self.oPSelectorLabels = []
-        self.selectOPFuncs = []
-        self.oPTraceFuncs = []
         for i in range(5):
             self.oPStringVars.append(tk.StringVar())
-            self.oPStringVars[-1].set("default")
-            self.oPSelectorLabels.append(tk.Label(OPMaker,
+            self.oPStringVars[i].set("default")
+            self.oPSelectorLabels.append(tk.Label(self.OPMaker,
                                          text="Select Function"))
-            self.selectOPFuncs.append(tk.OptionMenu(OPMaker,
-                                                    self.oPStringVars[-1],
+            self.selectOPOptionMenus.append(tk.OptionMenu(self.OPMaker,
+                                                    self.oPStringVars[i],
                                                     *self.userFuncs.keys()))
-            #def updateOption(*args):
-
+            self.selectOPOptionMenus[i].grid(row=i+1, column = 1)
             
-        
-        
-        
-        self.selectVFieldFunc = tk.OptionMenu(OPMaker, vFieldFunc, 
-                                   *self.userFuncs.keys())
-        self.selectEObsFunc = tk.OptionMenu(OPMaker, EObsFunc, 
-                                   *self.userFuncs.keys())
-        self.selectHolDetectFunc = tk.OptionMenu(OPMaker, HolDetectFunc, 
-                                   *self.userFuncs.keys())
-        self.selectSurveyNoiseFunc = tk.OptionMenu(OPMaker, SurveyNoiseFunc, 
-                                   *self.userFuncs.keys())
-        self.selectMeasureFunc = tk.OptionMenu(OPMaker, MeasureFunc, 
-                                   *self.userFuncs.keys())
-        vFieldSelectorLabel = tk.Label(OPMaker, 
+
+        vFieldSelectorLabel = tk.Label(self.OPMaker, 
                             text="Select Viewing Field Function")
-        eObsSelectorLabel = tk.Label(OPMaker, 
+        eObsSelectorLabel = tk.Label(self.OPMaker, 
                             text="Select Extra Obstruction Function")
-        hDetectSelectorLabel = tk.Label(OPMaker, 
+        hDetectSelectorLabel = tk.Label(self.OPMaker, 
                             text="Select holistic detection Function")
-        sNoiseSelectorLabel = tk.Label(OPMaker, 
+        sNoiseSelectorLabel = tk.Label(self.OPMaker, 
                             text="Select survey noise Function")
-        mFuncSelectorLabel = tk.Label(OPMaker, 
+        mFuncSelectorLabel = tk.Label(self.OPMaker, 
                             text="Select Measurement Function")
         
         
         loadFile.grid(row=0, column = 0)
-        self.selectVFieldFunc.grid(row=1, column = 1)
-        self.selectEObsFunc.grid(row=2, column = 1)
-        self.selectHolDetectFunc.grid(row=3, column = 1)
-        self.selectSurveyNoiseFunc.grid(row=4, column = 1)
-        self.selectMeasureFunc.grid(row=5, column = 1)
         vFieldSelectorLabel.grid(row=1, column = 0)
         eObsSelectorLabel.grid(row=2, column = 0)
         hDetectSelectorLabel.grid(row=3, column = 0)
@@ -188,18 +171,6 @@ class CCBias():
         def nothing(*args):
             pass
         
-        def updateVFieldOptionMenus(*args):
-            """Recreate the selectFunc Option Menu"""
-            temp = vFieldFunc.get()
-            self.selectVFieldFunc.destroy()
-            self.selectVFieldFunc = tk.OptionMenu(OPMaker, vFieldFunc, 
-                                    *self.userFuncs.keys())
-            self.selectVFieldFunc.grid(row=1, column = 1) 
-            vFieldFunc.trace('w', nothing)
-            vFieldFunc.set(temp)
-            vFieldFunc.trace('w', updateVFieldOptionMenus)
-        vFieldFunc.trace('w', updateVFieldOptionMenus)
-        
         def addFile(path):
             """Open a file selection dialogue and update options"""
             self.paths.append(path)
@@ -208,9 +179,18 @@ class CCBias():
             self.userFiles.append(importlib.util.module_from_spec(spec))
             spec.loader.exec_module(self.userFiles[-1])
             self.updateUserFunctionsDict()
-            updateVFieldOptionMenus()
+            self.updateOption()
 
-
+    def nothing(self, *args):
+        pass
+    
+    def updateOption(self, *args):
+        for i in range(5):
+            self.selectOPOptionMenus[i].destroy()
+            self.selectOPOptionMenus[i] = tk.OptionMenu(self.OPMaker,
+                                                    self.oPStringVars[i],
+                                                    *self.userFuncs.keys())
+            self.selectOPOptionMenus[i].grid(row=i+1, column = 1)
 
     def updateUserFunctionsDict(self):
         
